c83eec6e5d3b095c1c619c5bea146f279043a0e1
grasielarosa <grasielarosaa@gmail.com>
2021-04-27 23:14:09 +0200

    ejercicios 1 y 2

---
+++ script.js
@@ -0,0 +1,21 @@
+// const lista = document.querySelector('ul');
+// let items = "";
+// for(let i=0; i<5; i++) {
+
+//   const cancion=prompt('Ingrese una canción');
+//   items+=`<li> ${cancion} </li>`;
+// }
+// lista.innerHTML=items;
+
+// let kilometros = document.getElementById('kilometros');
+// let millas = document.getElementById('millas');
+// const calculo = (event) => {
+//     if (event.target.name === 'kilometros') {
+//         millas.value = kilometros.value * 1.61;
+//     } else if (event.target.name === 'millas') {
+//         kilometros.value = millas.value * 0.62;
+//     }
+// }
+// millas.addEventListener('change', calculo);
+// kilometros.addEventListener('change', calculo); 
+
